b39963d59012d0fba36bcd7fd991b7e88a5c1f60
kshetritej <tejghartikshetri@gmail.com>
2026-04-12 11:45:50 +0545

    chore(fix): build issue

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -27,7 +27,7 @@ export default async function BlogPage() {
       {/* Grid */}
       {blogs?.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {blogs?.map((blog) => (
+          {blogs?.map((blog: any) => (
             <BlogCard key={blog.id} blog={blog} />
           ))}
         </div>
